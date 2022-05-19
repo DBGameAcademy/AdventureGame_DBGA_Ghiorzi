694b7e233731925d7466994051a0076724491e0a
Cristian <cri.ghiorzi@gmail.com>
2022-05-19 11:44:53 +0200

    HealthBar animation

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -8,6 +8,10 @@ public class HealthBar : MonoBehaviour
 {
     private Slider _slider;
 
+    private float _valueToChange;
+    private float _speedChange = 2f;
+    private bool _isChanging;
+
     public void SetUp(int maxValue, int currentValue)
     {
         if (currentValue > maxValue)
@@ -19,11 +23,29 @@ public class HealthBar : MonoBehaviour
 
     public void SetValue(int value)
     {
-        _slider.value = value;
+        //_slider.value = value;
+        _valueToChange = value;
+        _isChanging = true;
     }
 
     private void Awake()
     {
         _slider = GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        if (_isChanging)
+        {
+            if(_slider.value > _valueToChange)
+            {
+                _slider.value -= Time.deltaTime*_speedChange;
+            }
+            else
+            {
+                _isChanging = false;
+            }
+        }
+
     }
 }
